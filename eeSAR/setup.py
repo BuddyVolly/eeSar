d6b5f7f75ed5620210421c90006d01b705c0d4d2
BuddyVolly <andreasvollrath@gmx.de>
2020-04-10 15:02:51 +0200

    add setup for pip install

--- eeSAR/setup.py
+++ eeSAR/setup.py
@@ -1,15 +1,4 @@
-import os
 from setuptools import setup, find_packages
-
-
-def parse_requirements(file):
-    return sorted(set(
-        line.partition('#')[0].strip()
-        for line in open(os.path.join(os.path.dirname(__file__), file))
-    )
-                  -set('')
-                  )
-
 
 setup(
     name='eeSar',
@@ -17,7 +6,6 @@ setup(
     include_package_data=True,
     version='0.0.1',
     description='Helper functions for SAR processsing with GEE',
-    install_requires=parse_requirements('requirements.txt'),
     url='https://github.com/BuddyVolly/eeSAR',
     author='Andreas Vollrath',
     author_email='andreas.vollrath[at]esa.int',
