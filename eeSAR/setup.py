3aa878be15ccfa3bc1bbe0be5943bd0020ba679a
BuddyVolly <andreasvollrath@gmx.de>
2020-04-10 14:52:00 +0200

    add setup for pip install

---
+++ eeSAR/setup.py
@@ -0,0 +1,29 @@
+import os
+from setuptools import setup, find_packages
+
+
+def parse_requirements(file):
+    return sorted(set(
+        line.partition('#')[0].strip()
+        for line in open(os.path.join(os.path.dirname(__file__), file))
+    )
+                  -set('')
+                  )
+
+
+setup(
+    name='eeSAR',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.0.1',
+    description='Helper functions for SAR rpocesssing with GEE',
+    install_requires=parse_requirements('requirements.txt'),
+    url='https://github.com/BuddyVolly/eeSAR',
+    author='Andreas Vollrath, Petr Sevcik',
+    author_email='andreas.vollrath[at]esa.int',
+    license='MIT License',
+    keywords=['Sentinel-1', 'ESA', 'SAR', 'Radar',
+              'Earth Observation', 'Remote Sensing',
+              'Synthetic Aperture Radar', 'Google Earth Engine'],
+    zip_safe=False,
+)
